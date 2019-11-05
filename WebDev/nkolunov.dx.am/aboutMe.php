a981ae805ca1b15c7632556abdd64e9624b2c640
Nikolai <angryniko@gmail.com>
2019-11-05 18:08:05 +0000

    Add files via upload

---
+++ WebDev/nkolunov.dx.am/aboutMe.php
@@ -0,0 +1,15 @@
+<?php include("header.php"); ?>
+		<div id="content">
+			<div id="navButtons">
+       			<?php include("navbuttons.php"); include("adminUpdateButton.php") ?><br/>
+			</div>
+
+			<p>
+				Hello, my name is Nikolai Kolunov and this is my little web-site (Still under construction, so pardon my dust.). I am a graduate CS student from Maynooth University.<br/>
+				I currently work at IBM as a Software Engineer in the Analytics team. Our main products are in scope of machine learning and data analytics.<br/>
+				If you have any questions (technical, professional or any other) email me at: <a href="mailto:nikolaykolunov@gmail.com">nikolaykolunov@gmail.com</a>
+			</p>
+
+		</div>
+	
+<?php include("footer.php"); ?>
