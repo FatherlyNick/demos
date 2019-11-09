8047c9c142151af6bb295a3d3705759e3fd39ccf
Nikolai <angryniko@gmail.com>
2019-11-09 20:30:23 +0000

    using Angular to control the input type

--- WebDev/ORTut/js/controllers/tutorialCtrl.js
+++ WebDev/ORTut/js/controllers/tutorialCtrl.js
@@ -5,7 +5,7 @@ angular.module('tutorialCtrlMod', [])
 	$scope.tutObject = {};
 	$scope.tutObject.str1 = "Str1";
 	$scope.tutObject.str2 = "Str2";
-	$scope.tutObject.number = '2';
+	$scope.tutObject.number = 2;
 	
 	$scope.timesTwo = function(){
 		$scope.tutObject.number *=2;
