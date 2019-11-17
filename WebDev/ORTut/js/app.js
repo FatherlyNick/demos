0bbf54067cd8dbff5fd480ae752d4d908e69c377
Nikolai <angryniko@gmail.com>
2019-11-17 21:55:19 +0000

    Added a list iterator controller
    Removed manual list, replaced with ng-repeat;
    Trying to make the page title and header dynamic, depending on which page is open.

--- WebDev/ORTut/js/app.js
+++ WebDev/ORTut/js/app.js
@@ -1,5 +1,27 @@
 var app = angular.module('tutorialApp', ["ngRoute","tutorialCtrlMod"]);
 
+app.controller("HomeController", ["$scope", function($scope){
+    $scope.page = {};
+    $scope.page.name = "!PLACEHOLDER!";
+    $scope.page.Home = "Home";
+    $scope.page.sp = "Second Page";
+    $scope.page.tp = "Third Page";
+
+}]);
+
+app.controller("ListCtrl", ["$scope", function($scope) {
+
+    $scope.myItems = [
+
+        { itemName: 'Item 1'},
+        { itemName: 'Item 2'},
+        { itemName: 'Item 3'},
+        { itemName: 'Item 4'},
+        { itemName: 'Item 5'}
+    ]
+
+}]);
+
 app.config(function ($routeProvider) {
     
     $routeProvider
